9e0767e33a9a48cc55b97fa6aeb4a3e728ad162d
C457 <android.c357@gmail.com>
2015-06-04 15:42:03 -0500

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# inherit from common g4stylus
+-include device/lge/g4stylus-common/BoardConfigCommon.mk
+
+TARGET_OTA_ASSERT_DEVICE := g4stylus,h631
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/h631/bluetooth
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_h631_defconfig
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2692743168
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
+
+# inherit from the proprietary version
+-include vendor/lge/h631/BoardConfigVendor.mk
